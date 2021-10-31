e685a5d4452c592a33a05be98b9d0676a05548f7
Matheus Eufrasio <matheuseufrasio2@gmail.com>
2021-10-31 01:25:17 -0300

    Base da rota de categories criada

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World" });
-});
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () => {
   console.log("🚀 Server is running on port 3333!");
