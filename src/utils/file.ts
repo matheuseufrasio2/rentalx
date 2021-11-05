225b652167d209d4dd4a71e1052f4382a6ca232c
Matheus Eufrasio <matheuseufrasio2@gmail.com>
2021-11-05 14:58:26 -0300

    upload de avatar realizado

---
+++ src/utils/file.ts
@@ -0,0 +1,12 @@
+/* eslint-disable no-useless-return */
+import fs from "fs";
+
+export const deleteFile = async (filename: string) => {
+  try {
+    await fs.promises.stat(filename);
+  } catch (err) {
+    return;
+  }
+
+  await fs.promises.unlink(filename);
+};
