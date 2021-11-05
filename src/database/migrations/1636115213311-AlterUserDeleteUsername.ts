3913f017f24fc15163ba9cccee7a010f01a71f77
Matheus Eufrasio <matheuseufrasio2@gmail.com>
2021-11-05 14:15:04 -0300

    finalização de autenticação

---
+++ src/database/migrations/1636115213311-AlterUserDeleteUsername.ts
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
+
+export class AlterUserDeleteUsername1636115213311
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn("users", "username");
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      "users",
+      new TableColumn({
+        name: "username",
+        type: "varchar",
+      })
+    );
+  }
+}
