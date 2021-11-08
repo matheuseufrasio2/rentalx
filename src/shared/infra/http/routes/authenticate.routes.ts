1885e1605452159510476558db433c4775e4e56a
Matheus Eufrasio <matheuseufrasio2@gmail.com>
2021-11-08 01:05:23 -0300

    inicio dos testes + refatoração/organização das pastas

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { AuthenticateUserController } from "../modules/accounts/useCases/autheticateUser/AuthenticateUserController";
+import { AuthenticateUserController } from "@modules/accounts/useCases/autheticateUser/AuthenticateUserController";
 
 const authenticateRoutes = Router();
 
