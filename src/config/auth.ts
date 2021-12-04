f976d1030c49a658e05e353f6929b1a8ecbc3cc5
Matheus Eufrasio <matheuseufrasio2@gmail.com>
2021-12-03 22:11:19 -0300

    refresh_token

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "ca66c5f25761ca4c1a555d7ce28ea117",
+  secret_refresh_token: "fbfc29fe6a16083131728f8a9392abe5",
+  expires_in_token: "15m",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
--- src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -27,6 +27,10 @@ class DayjsDateProvider implements IDateProvider {
   getDateNow(): Date {
     return dayjs().toDate();
   }
+
+  addDays(days: number): Date {
+    return dayjs().add(days, "days").toDate();
+  }
 }
 
 export { DayjsDateProvider };
